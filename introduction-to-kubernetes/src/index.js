7085a0669c802acae581eb48acbb929904e8441d
Gareth Luckett <gareth.luckett@gmail.com>
2020-05-22 15:26:34 +0100

    Added dockerfile and kubernetes templates

---
+++ introduction-to-kubernetes/src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const os = require("os");
+
+const app = express();
+
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
+    'hostname': os.hostname(),
+    'version': process.version
+  }));
+})
+
+app.listen(port, '0.0.0.0', () => {
+  console.log('App Started on port %s!', port)
+});
